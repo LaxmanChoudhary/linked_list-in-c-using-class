5e657ac65b97d7d361ebfa479524c584e252e6ec
laxman_choudhary <38493927+laxman6811@users.noreply.github.com>
2018-08-05 23:00:48 +0530

    Add files via upload

---
+++ singly_linked_list.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+//linked list with class
+
+class Node
+{
+ public:
+	int data;
+	Node *next;
+};
+
+class linked_list
+{
+ private:
+	Node *head;
+  	
+ public:
+ 	linked_list()
+ 	{
+ 		head = NULL;
+ 	}
+ 	
+ 	~linked_list() {};
+ 	void append_to(int);
+ 	void delete_to();
+ 	void display();
+};
+
+void linked_list :: append_to(int x)
+{
+	Node *cur = new Node;
+	cur->data = x;
+	if(head == NULL)
+	{
+		head = cur;
+		head->next = NULL;
+	}
+	else
+	{
+		Node *temp = head;
+		while(temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = cur;
+	}
+}
+
+void linked_list :: display()
+{
+	if(head == NULL)
+	{
+		cout<<"No elements to display";
+	}
+	else
+	{
+		Node *temp = head;
+		while(temp != NULL)
+		{
+			cout<<temp->data;
+			temp = temp->next;
+		}
+	}
+}
+
+int main()
+{
+	linked_list root;
+	root.append_to(6);
+	//root.append_to(9);
+	root.display();
+	return 0;	
+}
